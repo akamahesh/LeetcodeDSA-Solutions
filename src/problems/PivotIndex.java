7f30e7d707fbfdd749967c93b0c70e2ce656e3f6
Mahesh Bhatt <mckay1718@gmail.com>
2022-06-30 13:10:32 -0700

    enhance: PivotIndex problem solution

---
+++ src/problems/PivotIndex.java
@@ -0,0 +1,19 @@
+package problems;
+
+import java.util.HashSet;
+
+public class PivotIndex {
+
+    public int pivotIndex(int[] nums) {
+        int sum = 0, leftsum = 0;
+        HashSet<Character> set = new HashSet<>();
+        for (int x : nums) {
+            sum += x;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
+        }
+        return -1;
+    }
+}
